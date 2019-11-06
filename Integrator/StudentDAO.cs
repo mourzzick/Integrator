5ca615a3abaa7047a9335225ca79ae9ecce9020a
Ruslan Georgiev <ruslan.georgiev@hotmail.com>
2019-11-06 23:05:39 +0100

    Add StudentDAO

---
+++ Integrator/StudentDAO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integrator
+{
+    class StudentDAO
+    {
+        public static List<Student> GetStudents()
+        {
+            List<Student> students = new List<Student>();
+            students.Add(new Student { Ideal = "rusgoe-7", FirstName = "Ruslan", LastName = "Georgiev", NationalIdentificationNumber = "8301281234" });
+            students.Add(new Student { Ideal = "cargra-7", FirstName = "Carl", LastName = "Granström", NationalIdentificationNumber = "8606251234" });
+            students.Add(new Student { Ideal = "josgus-8", FirstName = "Josefine", LastName = "Gustafsson", NationalIdentificationNumber = "9007091234" });
+            students.Add(new Student { Ideal = "frehar-7", FirstName = "Fredrik", LastName = "Harnevik", NationalIdentificationNumber = "8701151234" });
+            students.Add(new Student { Ideal = "sebhay-7", FirstName = "Sebastian", LastName = "Häyry", NationalIdentificationNumber = "8506241234" });
+            students.Add(new Student { Ideal = "sebhel-7", FirstName = "Sebastian", LastName = "Hellström", NationalIdentificationNumber = "7906211234" });
+            students.Add(new Student { Ideal = "filhuh-7", FirstName = "Filip", LastName = "Huhta", NationalIdentificationNumber = "8809151234" });
+            return students;
+        }
+    }
+}
