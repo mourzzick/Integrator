69b7ccac2e56c582ffef3d222d012a6fb8b3e60c
Ruslan Georgiev <ruslan.georgiev@hotmail.com>
2019-11-06 22:40:36 +0100

    Add Student class.

---
+++ Integrator/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integrator
+{
+    public class Student
+    {
+        public string Ideal { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string NationalIdentificationNumber { get; set; }
+        public string CourseRegistrationNumber { get; set; }
+        public Assignment Assignment { get; set; }
+    }
+}
