c70f1dd476f143643155d18874981cb4991e98ed
Ruslan Georgiev <ruslan.georgiev@hotmail.com>
2019-11-06 22:39:02 +0100

    Add Assignment class.

---
+++ Integrator/Assignment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integrator
+{
+    public class Assignment
+    {
+        public string AssignmentId { get; set; }
+        public string AssignmentGrade { get; set; }
+    }
+}
