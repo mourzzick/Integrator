500e447963ec838a5dc2b86274f527d4b009ba02
Ruslan Georgiev <ruslan.georgiev@hotmail.com>
2019-11-07 21:51:18 +0100

    Add ApplicationCodeProcessor class.

--- Integrator/ApiHelper.cs
+++ Integrator/ApiHelper.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Integrator
 {
-    class ApiHelper
+    public static class ApiHelper
     {
+        public static HttpClient httpClient { get; set; }
+
+        public static void InitializeClient()
+        {
+            httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
     }
 }
---
+++ Integrator/ApplicationCodeProcessor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integrator
+{
+    class ApplicationCodeProcessor
+    {
+        public async Task<string> LoadApplicationCode(string courseCode, string semester)
+        {
+            string url = string.Empty;
+            if (!string.IsNullOrEmpty(courseCode) && !string.IsNullOrEmpty(semester))
+            {
+                url = $"http://localhost:3005/epok/api/registration-code/{courseCode}-{semester}";
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            using (HttpResponseMessage response = await ApiHelper.httpClient.GetAsync(url))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    // TODO: finish this
+                }
+            }
+
+            throw new NotImplementedException();
+             
+        }
+        
+        // "registration_code": "ltu-22008",
+        // "course_code": "d0031n",
+        // "semester": "ht16"
+    }
+}
