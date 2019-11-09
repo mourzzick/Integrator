05cfae5758a64f58a0a25aa6952dac5e49385a55
Ruslan Georgiev <ruslan.georgiev@hotmail.com>
2019-11-09 10:45:53 +0100

    Add ApplicationCodeResponse to hold application code information from the Epok service.

---
+++ Integrator/ApplicationCodeResponse.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integrator
+{
+    public class ApplicationCodeResponse
+    {
+        [JsonProperty("registration_code")]
+        public string ApplicationCode { get; set; }
+
+        [JsonProperty("course_code")]
+        public string CourseCode { get; set; }
+
+        [JsonProperty("semester")]
+        public string Semester { get; set; }
+    }
+}
