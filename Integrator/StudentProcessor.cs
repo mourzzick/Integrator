e34eb67f6ce9aa9475c70a8f44e78c02665c8b40
Ruslan Georgiev <ruslan.georgiev@hotmail.com>
2019-11-09 12:47:08 +0100

    Add StudentProcessor to connect to the Ideal API and get students by application code.

---
+++ Integrator/StudentProcessor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integrator
+{
+    class StudentProcessor
+    {
+        public async Task<List<Student>> GetStudentsAsync(string applicationCode)
+        {
+            string url = string.Empty;
+
+            if (string.IsNullOrEmpty(applicationCode))
+            {
+                throw new ArgumentOutOfRangeException("Application code should not be null or empty.");
+            }
+
+            url = $" http://localhost:3010/ideal/v1/course/{applicationCode}/students";
+
+            using (HttpResponseMessage response = await ApiHelper.httpClient.GetAsync(url))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    List<Student> students = await response.Content.ReadAsAsync<List<Student>>();
+                    return students;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+    }
+}
